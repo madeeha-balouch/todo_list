37cc7fcc15bb9ea73124636024b3d99915684d5e
Madeeha Balouch <madeehabalouch26@gmail.com>
2024-09-06 21:02:42 +0400

    to-do list

---
+++ src/app/_components/new-todo-form.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+type ToDoFormProps = {
+    onCreate: (title:string, description:string) => void;
+}
+export function NewToDoForm({onCreate}: ToDoFormProps){
+    const [title, setTitle] = useState("");
+    const [description, setDesc] = useState("");
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        onCreate(title,description);
+        setTitle("");
+        setDesc("");
+  }
+
+    return (
+        <form onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-2">
+      <label className="text-sm font-semibold" htmlFor="title">Title</label>
+      <input className="p-1 border rounded"
+      type="text" name="title" id="title" value={title} onChange={e => setTitle(e.target.value)}/>
+      <label className="text-sm font-semibold"htmlFor="description">Description</label>
+      <input className="p-1 border rounded"
+       type="text" name="description" id="description" value={description} onChange={e => setDesc(e.target.value)}/>
+      <button className="bg-blue-500 p-1 rounded text-white" type="submit">Create</button>
+      </div>
+     </form>
+    )
+}
