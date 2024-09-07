d78397fc8b962a3897b3cb0db2e13b8e3b21c999
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-09-07 11:16:44 +0400

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,8 @@
 'use client';
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
 
 const client = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 export function ConvexClientProvider({children}: {children: React.ReactNode}){
