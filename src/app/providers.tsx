a2713920167fa44785383413ab25a2259295d95d
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-09-07 11:19:47 +0400

    convex-providers.tsx - providers.tsx
    Using ConvexProviderWithClerk to add Authentication of Clerk with Convex.

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,14 +1,16 @@
 'use client';
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexReactClient } from "convex/react";
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 
 const client = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
-export function ConvexClientProvider({children}: {children: React.ReactNode}){
+export function Providers({ children }: { children: React.ReactNode }) {
     return (
-        <ConvexProvider client={client}>
-            {children}
-        </ConvexProvider>
+        <ClerkProvider>
+            <ConvexProviderWithClerk client={client} useAuth={useAuth}>
+                {children}
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
     );
 }
